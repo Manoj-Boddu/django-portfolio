dcef2d3fe40f67d30d3d103c10ea74952cb608eb
nuttej <94890052+Unetha@users.noreply.github.com>
2025-06-28 23:13:12 +0530

    Ready for Render deployment

---
+++ internalHome/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Project Model
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+# BlogPost Model (use this one only)
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
